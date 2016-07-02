1a51f46c4d10cb6a92ee2633784bee4d42b740bd
eu2004 <utma.emilian@gmail.com>
2016-07-02 13:44:25 +0200

    initial commit for xlsxdb_app

---
+++ xlsxdb_app/src/main/java/ro/eu/xlsxdb/SpringApplicationLauncher.java
@@ -0,0 +1,41 @@
+package ro.eu.xlsxdb;
+
+import java.io.File;
+import java.nio.file.Paths;
+
+import org.apache.log4j.Logger;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import ro.eu.xlsxdb.ApplicationExecutor;
+import ro.eu.xlsxdb.database.XSLXTableDao;
+import ro.eu.xlsxdb.xlsxloader.XLSXLoader;
+
+/**
+ * Created by emilu on 5/21/2016.
+ */
+public class SpringApplicationLauncher {
+	private static final Logger logger = Logger.getLogger(SpringApplicationLauncher.class);
+
+	public static void main(String[] args) {
+		// check input arguments
+		if (args.length == 0) {
+			throw new IllegalArgumentException("Invalid argument exception. Use: load.bat <directory path>");
+		}
+
+		// extract parameters
+		String folderPath = args[0];
+		String action = args.length > 1 ? args[1] : "load";
+
+		// validate parameters value
+		File folder = Paths.get(folderPath).toFile();
+		if (!folder.exists() || !folder.isDirectory()) {
+			throw new IllegalArgumentException(folderPath + " is not valid");
+		}
+
+		// launch app
+		ApplicationContext context = new AnnotationConfigApplicationContext(SpringApplicationConfiguration.class);
+		ApplicationExecutor applicationExecutor = new ApplicationExecutor(logger, (XLSXLoader) context.getBean("XLSXLoader"), (XSLXTableDao) context.getBean("xslxTableDao"));
+		applicationExecutor.execute(folderPath, action);
+	}
+}
---
+++ xlsxdb_app/src/main/resources/startDB.bat
@@ -0,0 +1,1 @@
+java -cp hsqldb-2.3.2.jar org.hsqldb.Server -database.0 file:xslxDataSource -dbname.0 xdb
